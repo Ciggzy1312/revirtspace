9b534d140bf9b4f28e583b24e21c6e534d689ee8
Deepayan <deepayanmukherjee1312@gmail.com>
2022-07-28 20:44:05 +0530

    Added update task function
    Signed-off-by: Deepayan <deepayanmukherjee1312@gmail.com>

--- components/tile.js
+++ components/tile.js
@@ -6,6 +6,10 @@ export default function Tile({ task }) {
         await axios.delete(`/api/tasks/${task._id}`)
     }
 
+    const updateTask = async ()=>{
+        await axios.patch(`/api/tasks/${task._id}`)
+    }
+
     return (
         <div className="">
 
@@ -18,7 +22,7 @@ export default function Tile({ task }) {
                 <div className="bg-[#5c5c5c] w-full h-0.5"></div>
 
                 <div className="text-white font-medium px-5 py-5">
-                    <button className="bg-[#c620a7] px-12 py-1 rounded mr-10">Mark as completed</button>
+                    <button className="bg-[#c620a7] px-12 py-1 rounded mr-10" onClick={updateTask}>Mark as completed</button>
                     <button className="text-[#afafaf] hover:text-[#c620a7] px-6" onClick={deleteTask}>Delete</button>
                 </div>
             </div>
--- pages/api/tasks/[id].js
+++ pages/api/tasks/[id].js
@@ -5,8 +5,30 @@ export default async function handler(req, res) {
     if (req.method === 'DELETE') {
         return await deleteTask(req, res);
     }
+    else if(req.method === 'PATCH'){
+        return await updateTask(req, res);
+    }
     else {
         return res.status(405).json({ message: 'Method not allowed', success: false });
+    }
+}
+
+async function updateTask(req, res) {
+
+    const { id } = req.query;
+
+    try {
+        await connectDB;
+
+        const task = await Task.findByIdAndUpdate(id, {
+            status: true
+        });
+
+        res.status(200).json({ data: task })
+
+    } catch (error) {
+        console.log(error);
+        res.json({ error });
     }
 }
 
